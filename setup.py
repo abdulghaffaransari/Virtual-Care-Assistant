60d7f4fd6e996f4af7ebfdb78486e77d03fac8ec
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-12-05 20:21:26 +0100

    Template Created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Virtual-Care-Assistant',
+    version= '0.0.0',
+    author= 'Abdul Ghaffar Ansari',
+    author_email= 'abdulghaffaransari9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
